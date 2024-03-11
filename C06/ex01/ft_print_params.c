5ba018a223065d78976e65194b7469a0fc3197e1
Oshintha Sandaruwan <oshinthasandaruwan@gmail.com>
2024-03-11 16:56:43 +0200

    add C06

---
+++ C06/ex01/ft_print_params.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoshinth <aoshinth@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/25 14:17:19 by aoshinth          #+#    #+#             */
+/*   Updated: 2024/01/29 11:46:51 by aoshinth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	if (argc > 1)
+	{
+		while (i < argc)
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				j++;
+			}
+			write(1, argv[i], j);
+			i++;
+			write(1, "\n", 1);
+		}
+	}
+	else
+		return (0);
+}
---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoshinth <aoshinth@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/25 20:23:24 by aoshinth          #+#    #+#             */
+/*   Updated: 2024/01/28 19:14:11 by aoshinth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strcmp(char *str1, char *str2)
+{
+	int	i;
+
+	i = 0;
+	while (str1[i] == str2[i] && str1[i])
+		i++;
+	return (str1[i] - str2[i]);
+}
+
+void	ft_print_params(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			write (1, &(argv[i][j]), 1);
+			j++;
+		}
+		i++;
+		write (1, "\n", 1);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 2;
+	while (i < argc)
+	{
+		j = i;
+		while (ft_strcmp(argv[j], argv[j - 1]) < 0 && j > 1)
+		{
+			temp = argv[j - 1];
+			argv[j - 1] = argv[j];
+			argv[j] = temp;
+			j--;
+		}
+		i++;
+	}
+	ft_print_params(argc, argv);
+	return (0);
+}
